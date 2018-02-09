aadb224bc839e49307e8050b8f1a1e942df716c8
NAVEEN SAGGAM <thenaveensaggam@gmail.com>
2018-02-09 21:01:48 +0530

    Update 04_app.js

---
+++ AngularJS_Starter_Template/04_Angular_ng_app_ng_Controller/04_app.js
@@ -0,0 +1,16 @@
+var app = angular.module("likesApp" , []);
+app.controller("likeAppCtrl",function ($scope) {
+    $scope.likes = 0;
+    $scope.dislikes = 0;
+    $scope.total = 0;
+    $scope.likesFn = function() {
+        $scope.likes++;
+        $scope.total++;
+
+    };
+    $scope.dislikesFn = function() {
+        $scope.dislikes++;
+        $scope.total++;
+    };
+
+});
