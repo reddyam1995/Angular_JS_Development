36fc011ddd303e352e806c57843b44bfb9006148
NAVEEN SAGGAM <thenaveensaggam@gmail.com>
2018-03-08 19:22:59 +0530

    Update 11_app.js

--- AngularJS_Starter_Template/11_Angular_Todo_App/11_app.js
+++ AngularJS_Starter_Template/11_Angular_Todo_App/11_app.js
@@ -1,1 +1,21 @@
 
+angular.module("GroceryApp",[]).controller("GroceryAppCtrl",function () {
+    this.item = "";
+    this.editMode = false;
+    this.groceryList = [];
+    this.addItem = function () {
+        if(this.item !== ""){
+            this.groceryList.push(this.item);
+            this.item = "";
+        }
+    };
+    this.enableEditMode = function () {
+      this.editMode = true;
+    };
+    this.disableEditMode = function () {
+      this.editMode = false;
+    };
+    this.deleteItem = function (index) {
+        this.groceryList.splice(index,1);
+    };
+});
