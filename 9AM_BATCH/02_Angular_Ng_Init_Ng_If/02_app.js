0f11c58fedf607ca6ca688f4b52d405a6c88e7a3
thenaveensaggam <thenaveensaggam@gmail.com>
2018-07-03 12:45:16 +0530

    removed

--- 9AM_BATCH/02_Angular_Ng_Init_Ng_If/02_app.js
+++
@@ -1,7 +0,0 @@
-// Creation of Angular Module
-var app = angular.module('GreetApp',[]);
-
-// Creation of Angular Controller
-app.controller('GreetAppCtrl',function() {
-
-});
--- 9AM_BATCH/03_Angular_ng_model/03_app.js
+++
@@ -1,7 +0,0 @@
-// Creation of Angular Module
-var app = angular.module('FormApp',[]);
-
-// Creation of Angular Controller
-app.controller('FormAppCtrl',function() {
-
-});
