27a7ef799a30fb598d5fd894d570d71c3aa56407
thenaveensaggam <thenaveensaggam@gmail.com>
2018-07-09 10:35:40 +0530

    removed

--- 9AM_BATCH/AngularJS_Completed_Template/09_Angular_ng_show_ng_hide/09_app.js
+++
@@ -1,9 +0,0 @@
-// Creation of Module
-var app  = angular.module('HobbiesApp',[]);
-
-// Creation of Controller
-app.controller('HobbiesAppCtrl',function () {
-   this.eating = false;
-   this.coding = false;
-   this.sleeping = false;
-});
