092e96cc91da0bdb5e1904ee1e951ec1d9e1b5bd
NAVEEN SAGGAM <thenaveensaggam@gmail.com>
2018-08-23 12:55:46 +0530

    added

---
+++ 11aM_BATCH/14_Angular_Invoice_App/14_app.js
@@ -0,0 +1,50 @@
+// Create a Module
+var app = angular.module('InvoiceApp',[]);
+
+// Create a Controller
+app.controller('InvoiceAppCtrl',function() {
+    this.invoice = {
+        customer_data : {
+            name : 'Mr. Rajan Jain',
+            companyName : 'Jain Industries Inc',
+            address1 : 'Plot No: 112',
+            address2 : 'Hitech City , Hyderabad',
+            pincode : 500010
+        },
+        company_data : {
+            name : 'UiBrains Tech Labs',
+            companyName : 'UiBrains.com',
+            address1 : 'Plot No: 1471, Road No. 12',
+            address2 : 'Jubilee Hills , Hyderabad',
+            pincode : 500102
+        },
+        gst : 13,
+        items : [
+            {
+                description : '',
+                quantity : '',
+                cost : ''
+            }
+        ]
+    };
+    this.deleteItem = function(item) {
+        this.invoice.items.splice(this.invoice.items.indexOf(item),1);
+    };
+    this.addItem = function() {
+      this.invoice.items.push({
+          description : '',
+          quantity : '',
+          cost : ''
+      });
+    };
+    this.subTotal = function() {
+        var subTotal = 0;
+        this.invoice.items.forEach(function (item) {
+            subTotal += (item.quantity * item.cost);
+        });
+        return subTotal;
+    };
+    this.addGST = function() {
+       return this.subTotal() * (this.invoice.gst / 100);
+    };
+});
