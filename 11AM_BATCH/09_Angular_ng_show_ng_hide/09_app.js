34e14e7f28757abeb8b8cfa32b157ee88e3c63d5
NAVEEN SAGGAM <thenaveensaggam@gmail.com>
2018-08-24 17:31:32 +0530

    removed

--- 11AM_BATCH/09_Angular_ng_show_ng_hide/09_app.js
+++
@@ -1,9 +0,0 @@
-// Create a Module
-var app = angular.module('HobbiesSelectionApp',[]);
-
-// Create a Controller
-app.controller('HobbiesSelectionAppCtrl',function() {
-    this.eating = null;
-    this.coding = null;
-    this.sleeping = null;
-});
